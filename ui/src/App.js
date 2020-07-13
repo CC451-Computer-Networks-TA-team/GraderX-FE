7eadbc529c7723394eee63fba7cfdf6abfebbb93
amrable <amrfahhassan@gmail.com>
2020-07-13 16:00:45 +0200

    add basic button

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <AppBar >
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Album layout
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Button style={{'marginTop': 100}} variant="contained" color="primary">Evaluate</Button>
+
     </div>
   );
 }
