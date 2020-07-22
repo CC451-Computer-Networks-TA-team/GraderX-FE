eeab86c0b060f4af24eaefb014af5b6f5e57be4f
Hagar Usama <Hagar_Usama@outlook.com>
2020-07-22 15:12:42 +0200

    added uploader tests

---
+++ src/__tests__/Grader.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Grader from '../components/grader/Grader';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
+describe('Components Visibilty', () => {
+
+    it("LabSelector is the only visible component", () => {
+        const wrapper = shallow( < Grader / > );
+        expect(wrapper.find('FileUpload').exists()).toBeFalsy();
+        expect(wrapper.find('DownloadResult').exists()).toBeFalsy();
+        expect(wrapper.find('Status').exists()).toBeFalsy();
+        expect(wrapper.find('LabSelector').exists()).toBeTruthy();
+
+
+    });
+
+
+
+});
