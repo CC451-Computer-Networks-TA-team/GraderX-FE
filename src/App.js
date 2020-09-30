6c53c38ed4bd8e10f7fcde715a17a2c726cb27e4
Hagar Usama <Hagar_Usama@outlook.com>
2020-09-30 21:33:30 +0000

    clean code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 /* import AppHeader from "./components/layout/AppHeader";
 import Grader from "./components/grader/Grader"; */
 import CoursesPage from "./pages/Courses";
-import ResultsContainer from "./components/grader/ResultsContainer";
-
 /**
  *      <AppHeader />
       <Grader />
