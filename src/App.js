72a746fae972e71fa378fa6e3bf2839d510b6642
Hagar Usama <Hagar_Usama@outlook.com>
2020-10-15 04:04:29 +0200

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from "react";
-/* import AppHeader from "./components/layout/AppHeader";
-import Grader from "./components/grader/Grader"; */
 import CoursesPage from "./pages/Courses";
-/**
- *      <AppHeader />
-      <Grader />
- */
-
 function App() {
   return (
     <React.Fragment>
