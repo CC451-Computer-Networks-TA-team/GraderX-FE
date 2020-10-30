2d84a0cb6c8c93a03fbcc61a1cbc8a7d3798bb57
Hagar Usama <Hagar_Usama@outlook.com>
2020-10-30 14:37:54 +0200

    update uploader tests, fix label bug

--- src/__tests__/FileUpload.test.js
+++ src/__tests__/FileUpload.test.js
@@ -14,37 +14,62 @@ describe('Testing File input', () => {
         />);
     });
 
-    // <p> exists whenever the file is Invalid
     it("should not accept the file", () => {
-
         wrapper.find('input').simulate("change", {
             target: {
                 files: [{ name: 'dummyValue', type: 'unSupported type' }]
             }
         });
 
-        expect(wrapper.find('p').exists()).toBeTruthy();
+        //<p>Supported files: .rar / .7z / .zip</p>
+        expect(wrapper.find('p').length).toEqual(2);
     });
-    // bug detected: Unhandled Rejection
-    it("should not accept the file", () => {
+    
+    it("should accept the file", () => {
 
         wrapper.find('input').simulate("change", {
             target: {
                 files: [{ name: 'dummyValue', type: 'application/vnd.rar' }]
             }
         });
-        // expect(wrapper.find('Label').exists()).toBeTruthy();
+        
+        expect(wrapper.find('p').length).toEqual(1);
     });
 });
 
 describe('Components Visibilty and Existance', () => {
-    it("label should not initially exist", () => {
-        const wrapper = shallow(< FileUpload />);
-        expect(wrapper.find('Label').exists()).toBeFalsy();
-    });
-
     it("input should be initially invisible", () => {
         const wrapper = shallow(< FileUpload />);
         expect(wrapper.find('input').prop('hidden')).toBeTruthy()
     });
+});
+
+describe('Extension Validation', () => {
+
+
+    it("should accept the extension", () => {
+        let extension = validateExtension("application/zip");
+        expect(extension).toBeTruthy();
+
+        extension = validateExtension("application/vnd.rar");
+        expect(extension).toBeTruthy();
+
+        extension = validateExtension("application/x-7z-compressed");
+        expect(extension).toBeTruthy();
+
+    });
+
+    it("should reject the extension", () => {
+        let extension = validateExtension("text/plain");
+        expect(extension).toBeFalsy();
+
+        extension = validateExtension("text/x-python");
+        expect(extension).toBeFalsy();
+
+        extension = validateExtension("anything");
+        expect(extension).toBeFalsy();
+
+    });
+
+
 });
