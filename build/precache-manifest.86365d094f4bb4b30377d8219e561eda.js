cd093edbf6975865c918e246e83af41223de1404
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-11-21 20:21:36 +0200

    generated build files

---
+++ build/precache-manifest.86365d094f4bb4b30377d8219e561eda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea31962c93dfa909fad76f4768f65f3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a9441845214c4fbe68",
+    "url": "/static/css/main.8512a746.chunk.css"
+  },
+  {
+    "revision": "1948bd8058f310a79987",
+    "url": "/static/js/2.12fe5d7e.chunk.js"
+  },
+  {
+    "revision": "91afdc6524c8210bee8ad67c0800beb3",
+    "url": "/static/js/2.12fe5d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a9441845214c4fbe68",
+    "url": "/static/js/main.77eaf49f.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  }
+]);
