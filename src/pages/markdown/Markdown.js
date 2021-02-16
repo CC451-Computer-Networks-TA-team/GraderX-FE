04367af82aa8d92a3d4be01f39931a1688b206f9
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2021-02-17 00:31:10 +0200

    added pytest support for single submission (#65)
    * added pytest support for single submission
    
    * Update Markdown.scss

--- src/pages/markdown/Markdown.js
+++ src/pages/markdown/Markdown.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
-import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm';
+import React, { useState, useEffect, useRef } from "react";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 import apiClient from "../../api-client";
 import ReactDiffViewer from "react-diff-viewer";
+import DownloadResult from "../../components/grader/DownloadResult";
 
-import './Markdown.scss';
+import "./Markdown.scss";
 
-import { Button } from 'carbon-components-react';
-import { Add16 } from '@carbon/icons-react';
+import { Button } from "carbon-components-react";
+import { Add16 } from "@carbon/icons-react";
 
 export function validateExtension(extension) {
   const supportedTypes = [
@@ -19,47 +20,48 @@ export function validateExtension(extension) {
   return supportedTypes.includes(extension);
 }
 
-
 function MarkdownPage({ match }) {
   const courseName = match.params.course;
   const LabName = match.params.lab;
 
   const fileInputRef = React.createRef();
   const [markdown, setMarkdown] = useState("");
-  const [resultsReady, setResultsReady] = useState(false)
-  const [diff, setDiff] = useState([])
-  const [totalTestcases, setTotalTestcases] = useState(0)
-  const resultsContainerRef = useRef(null)
+  const [resultsReady, setResultsReady] = useState(false);
+  const [diff, setDiff] = useState([]);
+  const [totalTestcases, setTotalTestcases] = useState(0);
+  const resultsContainerRef = useRef(null);
 
   useEffect(() => {
-    apiClient.getLabMD(courseName, LabName).then(res => {
+    apiClient.getLabMD(courseName, LabName).then((res) => {
       setMarkdown(res.data.lab_guide);
     });
   });
 
   useEffect(() => {
-    if(resultsContainerRef.current && resultsReady){
-      resultsContainerRef.current.scrollIntoView()
+    if (resultsContainerRef.current && resultsReady) {
+      resultsContainerRef.current.scrollIntoView();
     }
-  }, [resultsReady])
+  }, [resultsReady]);
 
   const uploadFile = (file) => {
+    setResultsReady(false);
     const formData = new FormData();
 
     formData.append("submissions_file", file);
 
-    apiClient
-      .uploadSubmissions(courseName, LabName, formData)
-      .then(_res => {
-        apiClient.startGrading(courseName, LabName, true)
-          .then(_res => {
-            apiClient.getDiffResults(courseName, LabName).then(res => {
-              setDiff(res.data.diff[0].failed);
-              setTotalTestcases(res.data.total_test_cases_count)
-              setResultsReady(true)
-            });
-          })
-      });
+    apiClient.uploadSubmissions(courseName, LabName, formData).then((_res) => {
+      apiClient
+        .startGrading(courseName, LabName, true)
+        .then((_res) => {
+          apiClient.getDiffResults(courseName, LabName).then((res) => {
+            setDiff(res.data.diff[0].failed);
+            setTotalTestcases(res.data.total_test_cases_count);
+          });
+        })
+        .finally((res) => {
+          setResultsReady(true);
+        });
+    });
   };
 
   function onFileChange(event) {
@@ -75,9 +77,7 @@ function MarkdownPage({ match }) {
     <div className="content single-submission">
       <div style={{ height: 24 }}></div>
       <div className="markdown-container">
-        <ReactMarkdown plugins={[gfm]}>
-          {markdown}
-        </ReactMarkdown>
+        <ReactMarkdown plugins={[gfm]}>{markdown}</ReactMarkdown>
       </div>
       <div style={{ height: 32 }}></div>
       <Button renderIcon={Add16} onClick={() => fileInputRef.current.click()}>
@@ -92,36 +92,42 @@ function MarkdownPage({ match }) {
         accept=".zip,.rar,.7zip,.tar,.7z"
       />
       <div style={{ height: 32 }}></div>
-      {
-        resultsReady && 
-        (
-            <div ref={resultsContainerRef} className="results-container">
-              <h3 className="results-header">Results</h3>
-              <p className="results-summary">You passed {totalTestcases - diff.length} test case{(totalTestcases - diff.length) == 1 ? "" : 's'} out of {totalTestcases}</p>
-              <div>
-                  {
-                      diff &&
-                      diff.map((tc) => (
-                          <div key={tc.tc_id} >
-                              <h4>Test Case {tc.tc_id}</h4>
-                              <hr></hr>
-                              <ReactDiffViewer
-                                  leftTitle="Expected"
-                                  rightTitle="Output"
-                                  oldValue={tc.expected}
-                                  newValue={tc.output}
-                                  useDarkTheme={true}
-                                  className="single-diff-container"
-                              />
-                              <br></br>
-                          </div>
-                      ))
-
-                  }
+      {resultsReady && (
+        <div ref={resultsContainerRef} className="results-container">
+          <h3 className="results-header">Results</h3>
+          {diff.length > 0 && (
+            <div>
+              <p
+                className={`results-summary ${
+                  diff.length == 0 ? "success" : "warning"
+                }`}
+              >
+                You passed {totalTestcases - diff.length} test case
+                {totalTestcases - diff.length == 1 ? "" : "s"} out of{" "}
+                {totalTestcases}
+              </p>
+              {diff.map((tc) => (
+              <div key={tc.tc_id}>
+                <h4>Test Case {tc.tc_id}</h4>
+                <hr></hr>
+                <ReactDiffViewer
+                  leftTitle="Expected"
+                  rightTitle="Output"
+                  oldValue={tc.expected}
+                  newValue={tc.output}
+                  useDarkTheme={true}
+                  className="single-diff-container"
+                />
+                <br></br>
               </div>
+              ))}
             </div>
-        )
-      }
+          )}
+          <div>
+            <DownloadResult course={courseName} lab={LabName}></DownloadResult>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
