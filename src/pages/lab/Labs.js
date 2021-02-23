8340d010ba35e62e9bc9fb9d199a523fc3ded78a
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2021-02-23 05:40:16 +0200

    added lab link in labs table (#70)
    * added lab link in labs table
    
    * grading complete header

--- src/pages/lab/Labs.js
+++ src/pages/lab/Labs.js
@@ -3,6 +3,8 @@ import apiClient from "../../api-client";
 import LabForm from "./LabForm";
 import "./styles.scss";
 import AppHeader from "../../components/layout/AppHeader";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 import {
   //Link,
   DataTable,
@@ -12,7 +14,7 @@ import {
   Select,
   SelectItem,
 } from "carbon-components-react";
-import { Delete16, Edit16, Add16 } from "@carbon/icons-react";
+import { Delete16, Edit16, Add16, Copy16, Launch16 } from "@carbon/icons-react";
 const {
   Table,
   TableBody,
@@ -27,39 +29,45 @@ const {
 } = DataTable;
 
 function LabsPage() {
-  const [didMount, setDidMount] = useState(false)
+  const [didMount, setDidMount] = useState(false);
   const [labs, setLabs] = useState([]);
-  const [labsLoaded, setLabsLoaded] = useState(false)
+  const [labsLoaded, setLabsLoaded] = useState(false);
   const [courseIds, setCourseIds] = useState([]);
-  const [courseIdsLoaded, setCourseIdsLoaded] = useState(false)
+  const [courseIdsLoaded, setCourseIdsLoaded] = useState(false);
   const [selectedCourse, setSelectedCourse] = useState("");
   const [openModal, setOpenModal] = useState(false);
-  const [labToEdit, setLabToEdit] = useState(null)
+  const [labToEdit, setLabToEdit] = useState(null);
+  const [copied, setCopied] = useState(false);
 
   const createLabsObjects = (labs_array) => {
     let labs_objects = [];
 
     labs_array.forEach((lab, index, _arr) => {
       lab.id = `${index}`;
+      lab.lab_link = formLabLink(lab.name);
       labs_objects.push(lab);
     });
 
     setLabs(labs_objects);
   };
 
-  useEffect(() => {
-    if (didMount) {
-      setLabsLoaded(true)
-    }
-  // eslint-disable-next-line
-  }, [labs])
+  const formLabLink = (lab_name) => {
+    return `${window.origin}/courses/${selectedCourse}/labs/${lab_name}/lab_guide`;
+  };
 
   useEffect(() => {
     if (didMount) {
-      setCourseIdsLoaded(true)
+      setLabsLoaded(true);
     }
-  // eslint-disable-next-line
-  }, [courseIds])
+    // eslint-disable-next-line
+  }, [labs]);
+
+  useEffect(() => {
+    if (didMount) {
+      setCourseIdsLoaded(true);
+    }
+    // eslint-disable-next-line
+  }, [courseIds]);
 
   const createCoursesIds = (courses_array) => {
     let courses_ids = [];
@@ -70,34 +78,35 @@ function LabsPage() {
   };
 
   const fetchLabs = (course_name) => {
-    setLabsLoaded(false)
+    setLabsLoaded(false);
     apiClient.getLabs(course_name).then((res) => {
       createLabsObjects(res.data.labs);
-      setSelectedCourse(course_name);
-    })
+    });
   };
 
   const changeLabs = (event) => {
     const course_name = event.target.value;
-    fetchLabs(course_name);
+    setSelectedCourse(course_name);
   };
 
   useEffect(() => {
-    setDidMount(true)
+    setDidMount(true);
     apiClient.getCourses(true).then((res) => {
       createCoursesIds(res.data.courses);
-      fetchLabs(res.data.courses[0].name)
       setSelectedCourse(res.data.courses[0].name);
-    })
-  // eslint-disable-next-line
+    });
+    // eslint-disable-next-line
   }, []);
+
+  useEffect(() => {
+    if (selectedCourse) {
+      fetchLabs(selectedCourse);
+    }
+  }, [selectedCourse]);
 
   const addLab = (formData) => {
     apiClient
-      .addLab(
-        selectedCourse,
-        formData
-      )
+      .addLab(selectedCourse, formData)
       .then((res) => {
         fetchLabs(selectedCourse);
       })
@@ -106,14 +115,9 @@ function LabsPage() {
       });
   };
 
-
   const editLab = (labId, formData) => {
     apiClient
-      .editLab(
-        selectedCourse,
-        labId,
-        formData
-      )
+      .editLab(selectedCourse, labId, formData)
       .then((res) => {
         fetchLabs(selectedCourse);
       })
@@ -129,16 +133,15 @@ function LabsPage() {
   };
 
   const openEditLab = (labID) => {
-    let labWithLabID = labs.find(lab => lab.name === labID)
-    setLabToEdit(labWithLabID)
-    setOpenModal(true)
-  }
+    let labWithLabID = labs.find((lab) => lab.name === labID);
+    setLabToEdit(labWithLabID);
+    setOpenModal(true);
+  };
 
   const closeLabModal = () => {
-    setOpenModal(false)
-    setLabToEdit(null)
-  }
-
+    setOpenModal(false);
+    setLabToEdit(null);
+  };
 
   return (
     <div>
@@ -155,121 +158,156 @@ function LabsPage() {
         <div style={{ height: 16 }}></div>
         <h1>Browse Labs</h1>
         <div style={{ height: 40 }}></div>
-        {
-          courseIdsLoaded ?
-            (
-              <Select id="select-1" labelText="Select Course" onChange={changeLabs}>
-                {courseIds.map((course_id) => (
-                  <SelectItem key={course_id} value={course_id} text={course_id} />
-                ))}
-              </Select>
-            ) :
-            (
-              <SelectSkeleton style={{ width: 224 }} />
-            )
-        }
+        {courseIdsLoaded ? (
+          <Select id="select-1" labelText="Select Course" onChange={changeLabs}>
+            {courseIds.map((course_id) => (
+              <SelectItem key={course_id} value={course_id} text={course_id} />
+            ))}
+          </Select>
+        ) : (
+          <SelectSkeleton style={{ width: 224 }} />
+        )}
         <div style={{ height: 56 }}></div>
-        {
-          labsLoaded ?
-            (
-              <DataTable
-                rows={labs}
-                headers={[
-                  {
-                    key: "id",
-                    header: "Id",
-                  },
-                  {
-                    key: "name",
-                    header: "Name",
-                  },
-                  {
-                    key: "disable_internet",
-                    header: "Internet",
-                  },
-                  {
-                    key: "runtime_limit",
-                    header: "Runtime Limit",
-                  },
-                  {
-                    key: "actions",
-                    header: "Actions",
-                  },
-                ]}
-                render={({ rows, headers, onInputChange }) => (
-                  <TableContainer>
-                    <TableToolbar aria-label="data table toolbar">
-                      <TableToolbarContent>
-                        <TableToolbarSearch onChange={onInputChange} />
-                      </TableToolbarContent>
-                      <Button
-                        renderIcon={Add16}
-                        onClick={() => {
-                          setOpenModal(true);
-                        }}
-                      >
-                        Add Lab
-                    </Button>
-                    </TableToolbar>
-                    <Table>
-                      <TableHead>
-                        <TableRow>
-                          {headers.map((header) => (
-                            <TableHeader>{header.header}</TableHeader>
-                          ))}
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {rows.map((row) => (
-                          <TableRow>
-                            {row.cells.map((cell) => (
-                              <TableCell key={cell.id}>
-                                {cell.value === row.cells[2].value
-                                  ? cell.value
-                                    ? "Disabled"
-                                    : "Enabled"
-                                  : null}
-                                {cell.value === row.cells[4].value ? (
-                                  <span>
-                                    <Button
-                                      kind="ghost"
-                                      size="small"
-                                      hasIconOnly
-                                      renderIcon={Edit16}
-                                      iconDescription="Edit"
-                                      onClick={() => {
-                                        openEditLab(row.cells[1].value)
-                                      }}
-                                    />
-                                    <Button
-                                      kind="ghost"
-                                      size="small"
-                                      hasIconOnly
-                                      renderIcon={Delete16}
-                                      iconDescription="Delete"
-                                      onClick={() => {
-                                        deleteLab(row.cells[1].value);
-                                      }}
-                                    />
-                                  </span>
-                                ) : (
-                                    cell.value
-                                  )}
-                              </TableCell>
-                            ))}
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                )}
-              />
-            ) :
-            (
-              <DataTableSkeleton showHeader={false} />
-            )
-        }
+        {labsLoaded ? (
+          <DataTable
+            rows={labs}
+            headers={[
+              {
+                key: "id",
+                header: "Id",
+              },
+              {
+                key: "name",
+                header: "Name",
+              },
+              {
+                key: "disable_internet",
+                header: "Internet",
+              },
+              {
+                key: "runtime_limit",
+                header: "Runtime Limit",
+              },
+              {
+                key: "lab_link",
+                header: "Student Submission Link",
+              },
+              {
+                key: "actions",
+                header: "Actions",
+              },
+            ]}
+            render={({ rows, headers, onInputChange }) => (
+              <TableContainer>
+                <TableToolbar aria-label="data table toolbar">
+                  <TableToolbarContent>
+                    <TableToolbarSearch onChange={onInputChange} />
+                  </TableToolbarContent>
+                  <Button
+                    renderIcon={Add16}
+                    onClick={() => {
+                      setOpenModal(true);
+                    }}
+                  >
+                    Add Lab
+                  </Button>
+                </TableToolbar>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      {headers.map((header) => (
+                        <TableHeader>{header.header}</TableHeader>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow>
+                        {row.cells.map((cell) => (
+                          <TableCell key={cell.id}>
+                            {cell.value === row.cells[2].value
+                              ? cell.value
+                                ? "Disabled"
+                                : "Enabled"
+                              : null}
 
+                            {cell.info.header === "actions" ? (
+                              <span>
+                                <Button
+                                  kind="ghost"
+                                  size="small"
+                                  hasIconOnly
+                                  renderIcon={Edit16}
+                                  iconDescription="Edit"
+                                  onClick={() => {
+                                    openEditLab(row.cells[1].value);
+                                  }}
+                                />
+                                <Button
+                                  kind="ghost"
+                                  size="small"
+                                  hasIconOnly
+                                  renderIcon={Delete16}
+                                  iconDescription="Delete"
+                                  onClick={() => {
+                                    deleteLab(row.cells[1].value);
+                                  }}
+                                />
+                              </span>
+                            ) : cell.info.header === "lab_link" ? (
+                              <span class="lab-link-container">
+                                <a
+                                  class="lab-link"
+                                  href={cell.value}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  Go To Lab
+                                  <Button
+                                    class="lab-link-button"
+                                    kind="ghost"
+                                    size="small"
+                                    hasIconOnly
+                                    renderIcon={Launch16}
+                                    iconDescription="Go To Lab"
+                                  />
+                                </a>
+                                <CopyToClipboard
+                                  text={cell.value}
+                                  onCopy={() => setCopied(true)}
+                                >
+                                  <Button
+                                    class="lab-link-copy-button"
+                                    kind="ghost"
+                                    size="small"
+                                    hasIconOnly
+                                    renderIcon={Copy16}
+                                    iconDescription={
+                                      copied
+                                        ? "Copied To Clipbloard!"
+                                        : "Copy To Clipboard"
+                                    }
+                                    onBlur={() => {
+                                      setCopied(false);
+                                    }}
+                                  />
+                                </CopyToClipboard>
+                              </span>
+                            ) : (
+                              cell.value
+                            )}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            )}
+          />
+        ) : (
+          <DataTableSkeleton showHeader={false} />
+        )}
       </div>
     </div>
   );
